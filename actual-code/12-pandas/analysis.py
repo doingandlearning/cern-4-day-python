e6688a53947bf3a8de2c0d1597651cff369364b5
2301 Training <trn2301@pchret2301.cern.ch>
2024-10-16 16:56:18 +0200

    Adds latest code

--- actual-code/12-pandas/analysis.py
+++ actual-code/12-pandas/analysis.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import matplotlib.pyplot as plt
 s = pd.Series([1,2,3,4,5,6, 7])
 
 print(s.count())
@@ -13,7 +13,24 @@ df["COI"] = pd.Series(["France", "Italy", "Turkey", "Austria"])
 print(df)
 print(df.describe())
 
+gym_data = pd.read_csv("./gym_membership.csv", index_col="id")
+print(gym_data) # the head and the tail
+print(gym_data.dtypes) # sense check the data types
+print(gym_data.describe())
+print(gym_data.shape)
 
 
+print(gym_data['gender'])
 
+filtered_df = gym_data[gym_data['Age'] > 40]
+print(filtered_df[filtered_df['gender'] == "Male"])
 
+sorted_by_age = gym_data.sort_values(by="Age")
+print(sorted_by_age["Age"])
+
+group_abonoment_type = gym_data.groupby('abonoment_type')
+print(group_abonoment_type['Age'].mean())
+
+plt.figure(figsize=(4,4))
+group_abonoment_type['Age'].plot.hist(alpha=0.4)
+plt.show()
