1acb09fca989c2b4ebf321740dc108d5c2fab437
2301 Training <trn2301@pchret2301.cern.ch>
2024-10-17 14:38:30 +0200

    Adds latest code

--- actual-code/15-flask/server.py
+++ actual-code/15-flask/server.py
@@ -1,15 +1,20 @@
 from flask import Flask, jsonify, render_template
-
+from datetime import date
 app = Flask(__name__)  # convention - you can go off piste if you like
 
 data = {
     "players": [
         {"name": "Sewald", "score": 2},
-        {"name": "Marc", "score": 2},
+        {"name": "Marc", "score": 1},
         {"name": "Matthias", "score": 2},
-        {"name": "Albane", "score": 2},
+        {"name": "Albane", "score": 5},
     ]
 }
+
+@app.route("/date")
+def date_page():
+    today = date.today()
+    return render_template("date.html", today=today)
 
 @app.route('/api/data', methods=["GET"])
 def get_data():
